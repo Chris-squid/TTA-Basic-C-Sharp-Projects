4ebc759db6b3cc9228de76fb7bc7e49aef53dcfb
Chris Robinson <74204601+Chris-squid@users.noreply.github.com>
2021-03-22 08:22:00 -0700

    WhileBoolean

---
+++ BoolWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            while (x < 10)
+                
+            {
+                Console.WriteLine(x);
+                x++;
+                Console.ReadLine();
+            }
+        }
+    }
+}
