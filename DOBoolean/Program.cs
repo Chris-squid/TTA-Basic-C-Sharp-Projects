565ca5091df62e0641fdc7b3ccaf466bbe81ae39
Chris Robinson <74204601+Chris-squid@users.noreply.github.com>
2021-03-22 08:22:30 -0700

    DoWhileBoolean

---
+++ DOBoolean/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOBoolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = string.Empty;
+            do
+            {
+                Console.WriteLine("Enter your password: ");
+                password = Console.ReadLine();
+            }
+            while (password != "Pa$$w0rd");
+
+            Console.WriteLine("Correct!");
+            Console.ReadLine();
+              
+        }
+    }
+}
