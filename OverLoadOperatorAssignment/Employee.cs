00a5205e1d21bde6dc21749d9a17b94abb40c95d
Chris Robinson <74204601+Chris-squid@users.noreply.github.com>
2021-03-29 11:25:19 -0700

    OverLoadOperatorAssignment

---
+++ OverLoadOperatorAssignment/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOperatorAssignment
+{
+     class Employee
+    {
+        public int Id { get; set; } // declare prperty must be inside the class
+
+        public static bool operator ==(Employee x, Employee y) //overload operater "==", checks to see of employee ids are equal or not
+        {
+            return x.Id == y.Id;
+        }
+
+        public static bool operator !=(Employee x, Employee y)
+        {
+            return x.Id != y.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Employee Id = obj as Employee;
+            if (Id != null)
+            {
+                return Id == this;
+            }
+            return false;
+        }
+        public override int GetHashCode() // when using Equals in your methods or class its important to include GetHashCode
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ OverLoadOperatorAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOperatorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee n = new Employee(); // create an instance of a class, call methods, objects.
+            n.Id = 1001;
+
+            Employee n1 = new Employee();
+            n.Id = 1001;
+
+            Console.WriteLine(n == n1); //print results, making compairison with == operator
+            Console.ReadLine(); // allows us to read the results being printed to the console
+        }
+    }
+}
