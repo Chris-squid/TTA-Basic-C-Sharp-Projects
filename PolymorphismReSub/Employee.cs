ddb6b8ec226f0727e09baa73d059a1b4945fdbb6
Chris Robinson <74204601+Chris-squid@users.noreply.github.com>
2021-03-30 07:38:15 -0700

    ReSubmitForPolyMorphAssignment

---
+++ PolymorphismReSub/Employee.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+
+namespace PolymorphismReSub
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            string first = firstName;
+            string last = lastName;
+            Console.WriteLine($"My name is {first} {last} and . . .");
+            Console.ReadLine();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I Quit! Time for a change!");
+            Console.ReadLine();
+        }
+    }
+}
