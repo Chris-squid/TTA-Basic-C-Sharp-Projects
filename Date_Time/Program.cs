701d59ebbb3d835f9fa68f7cc9b62cc84e366640
Chris Robinson <74204601+Chris-squid@users.noreply.github.com>
2021-04-07 08:06:00 -0700

    DateTime

---
+++ Date_Time/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print the current date and time to the console.
+            DateTime currentDateTime = DateTime.Now;// here we are gettig the current dateTime by using the .Now .we can accsess many attributes of DateTime
+            Console.WriteLine("Current date and time: " + currentDateTime);// printing to the console, current date and time
+            //ask user to enter a number.
+            Console.WriteLine("Please enter a number.");
+            string number = Console.ReadLine();
+            //print the exact time it will be in X hours, X being the number enterd by our user.
+            DateTime futureDateTime = currentDateTime.AddHours(Convert.ToInt32(number));
+            Console.WriteLine(number + " hours from now it will be " + futureDateTime);
+            Console.ReadLine();
+        }
+    }
+}
